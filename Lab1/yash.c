e798d7f82e3d97df92da167dfa607ddda968bb47
varunsridhar1 <varun.sridhar@utexas.edu>
2017-09-17 16:53:44 -0500

    Substring working

--- Lab1/yash.c
+++ Lab1/yash.c
@@ -7,8 +7,8 @@
 #include <string.h>
 
 enum BUILTIN_COMMANDS { NO_SUCH_BUILTIN=0, FG, BG, JOBS };
-int run;
-int addargs;
+int run1, run2;
+int addargs1, addargs2;
 int pipefd[2];
 char* pipeCommand;
 int pipeFlag;
@@ -34,9 +34,8 @@ int main(int argc, char **argv) {
 	int fileNo;
 	while(1) {	
 		printf("%s ", "#");
-		run = 1;
-		addargs = 1;
-		pipeFlag = 0;
+		run1 = 1;
+		addargs1 = 1;
 		
 
 		fgets(cmd, 2001, stdin);
@@ -65,7 +64,7 @@ int main(int argc, char **argv) {
 						fileNo = fileno(fp);
 						dup2(fileNo, STDOUT_FILENO);
 						fclose(fp);
-						addargs = 0;
+						addargs1 = 0;
 					}
 					else if(*p == '<') {
 						p = strtok(NULL, " ");
@@ -77,12 +76,12 @@ int main(int argc, char **argv) {
 							int a = fscanf(fp, "%s", arg);
 							myargs[i++] = arg;
 							fclose(fp);
-							addargs = 0;
+							addargs1 = 0;
 						}
 						else {
 							fprintf(stderr, "%s %s\n", "Invalid file:", p);
-							addargs = 0;
-							run = 0;
+							addargs1 = 0;
+							run1 = 0;
 						}
 					}
 					else if(*p == '2' && *(++p) == '>') {
@@ -91,55 +90,100 @@ int main(int argc, char **argv) {
 						fileNo = fileno(fp);
 						dup2(fileNo, STDERR_FILENO);
 						fclose(fp);
-						addargs = 0;
+						addargs1 = 0;
 					}
 
 					else if(*p == '|') {
 						setsid();
 						close(pipefd[0]);
-						//dup2(pipefd[1], STDOUT_FILENO);
+						dup2(pipefd[1], STDOUT_FILENO);
 						pipeFlag = 1;
-						addargs = 0;
+						addargs1 = 0;
 						// set global substring with all text on right side of |
-						int index = 0;
-						while(cmd[index] != '|') {
-							index++;
-						}
-						index += 2;
-						printf("%d\n", index);
-						int len = index;
-						while(cmd[index] != '\0') {
-							len++;
-						}
-						len -= index;
-						strncpy(pipecmd, cmd + index, len);
-						printf("Pipe command: %s\n", pipecmd);
+						char* temp = strstr(cmd, "|");
+						pipecmd = strtok(temp, " ");
 
 					}
 
-					if(addargs) {
+					if(addargs1) {
 						myargs[i++] = p;
 					}
 					p = strtok(NULL, " ");
 				}	
 				myargs[i] = NULL;
-				if(run) {
+				if(run1) {
 					if(execvp(myargs[0], myargs) < 0) {
 						printf("%s: %s: %s\n", "yash", myargs[0], "command not found");
 					}
 				}
 			}
 			else {	// Parent
-				if(pipeFlag) {
+				if(pipeFlag) { 
 					cpid2 = fork();
-					if(cpid2 == 0) {
+					if(cpid2 == 0) { // child 2
 						// use global substring and loop through and put in args array
-						
-						//dup2(pipefd[0], STDIN_FILENO);
-						// execvp
+						close(pipefd[1]);
+						dup2(pipefd[0], STDIN_FILENO);
+						run2 = 1;
+						addargs2 = 1;
+						char **myargs = (char**) malloc(sizeof(char*) * 10);
+						int i = 0;
+						char *p = strtok(cmd, " ");
+				
+						while(p != NULL) {
+							if(*p == '>') {
+								p = strtok(NULL, " ");
+								FILE* fp = fopen(p, "w");
+								fileNo = fileno(fp);
+								dup2(fileNo, STDOUT_FILENO);
+								fclose(fp);
+								addargs2 = 0;
+							}
+							else if(*p == '<') {
+								p = strtok(NULL, " ");
+								FILE* fp = fopen(p, "r");
+								if(fp) {
+									//fileNo = fileno(fp);
+									//dup2(fileNo, STDIN_FILENO);
+									char* arg;
+									int a = fscanf(fp, "%s", arg);
+									myargs[i++] = arg;
+									fclose(fp);
+									addargs2 = 0;
+								}
+								else {
+									fprintf(stderr, "%s %s\n", "Invalid file:", p);
+									addargs2 = 0;
+									run2 = 0;
+								}
+							}
+							else if(*p == '2' && *(++p) == '>') {
+								p = strtok(NULL, " ");
+								FILE* fp = fopen(p, "w");
+								fileNo = fileno(fp);
+								dup2(fileNo, STDERR_FILENO);
+								fclose(fp);
+								addargs2 = 0;
+							}
+
+							if(addargs2) {
+								myargs[i++] = p;
+							}
+							p = strtok(NULL, " ");
+						}	
+						myargs[i] = NULL;
+						if(run2) {
+							if(execvp(myargs[0], myargs) < 0) {
+								printf("%s: %s: %s\n", "yash", myargs[0], "command not found");
+							}
+						}
+					}
+					else {
+						pipeFlag = 0;
+						wait(NULL);
 					}
 				}
-				if(run) {
+				if(run1) {
 					wait(NULL);
 				}
 				
