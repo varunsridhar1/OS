28481d3aa37f44cc703f4e7d592aafa91a6c9e26
Varun Sridhar <varun.sridhar@utexas.edu>
2017-09-17 21:40:44 -0500

    Pipes and multiple redirection working

--- Lab1/yash.c
+++ Lab1/yash.c
@@ -12,6 +12,12 @@ int addargs1, addargs2;
 int pipefd[2];
 char* pipeCommand;
 int pipeFlag;
+int outputRedirect1, outputRedirect2;
+int errorRedirect1, errorRedirect2;
+FILE* fp1;
+FILE* fp2;
+int cpid;
+int cpid2;
 
 int isBuiltInCommand(char *cmd) {
 	if(strncmp(cmd, "fg", strlen("fg")) == 0) {
@@ -29,14 +35,17 @@ int isBuiltInCommand(char *cmd) {
 int main(int argc, char **argv) {
 	char *cmd = (char*) malloc(sizeof(char)*1001);						// command to execute
 	char *pipecmd = (char*) malloc(sizeof(char)*20);
-	int cpid;
-	int cpid2;
-	int fileNo;
 	while(1) {	
 		printf("%s ", "#");
-		run1 = 1;
+		run1 = 1;		
+		run2 = 1;
 		addargs1 = 1;
-		
+		addargs2 = 1;
+		pipeFlag = 0;
+		outputRedirect1 = -1;
+		errorRedirect1 = -1;
+		outputRedirect2 = -1;
+		errorRedirect2 = -1;
 
 		fgets(cmd, 2001, stdin);
 		if(strcmp(cmd, "") != 0) {
@@ -51,144 +60,142 @@ int main(int argc, char **argv) {
 				perror("pipe");
 				exit(EXIT_FAILURE);
 			}
-			cpid = fork();
-			if(cpid == 0) {
-				char **myargs = (char**) malloc(sizeof(char*) * 10);
-				int i = 0;
-				char *p = strtok(cmd, " ");
+			char **myargs = (char**) malloc(sizeof(char*) * 10);
+			char **otherargs = (char**) malloc(sizeof(char*) * 10);
+			int i = 0;
+			char *p = strtok(cmd, " ");
 				
-				while(p != NULL) {
-					if(*p == '>') {
-						p = strtok(NULL, " ");
-						FILE* fp = fopen(p, "w");
-						fileNo = fileno(fp);
-						dup2(fileNo, STDOUT_FILENO);
+			while(p != NULL) {
+				if(*p == '>') {
+					p = strtok(NULL, " ");
+					fp1 = fopen(p, "w");
+					outputRedirect1 = fileno(fp1);
+					addargs1 = 0;
+				}
+				else if(*p == '<') {
+					p = strtok(NULL, " ");
+					FILE* fp = fopen(p, "r");
+					if(fp1) {
+						char* arg;
+						int a = fscanf(fp, "%s", arg);
+						myargs[i++] = arg;
 						fclose(fp);
 						addargs1 = 0;
 					}
-					else if(*p == '<') {
-						p = strtok(NULL, " ");
-						FILE* fp = fopen(p, "r");
-						if(fp) {
-							//fileNo = fileno(fp);
-							//dup2(fileNo, STDIN_FILENO);
-							char* arg;
-							int a = fscanf(fp, "%s", arg);
-							myargs[i++] = arg;
-							fclose(fp);
-							addargs1 = 0;
-						}
-						else {
-							fprintf(stderr, "%s %s\n", "Invalid file:", p);
-							addargs1 = 0;
-							run1 = 0;
-						}
-					}
-					else if(*p == '2' && *(++p) == '>') {
-						p = strtok(NULL, " ");
-						FILE* fp = fopen(p, "w");
-						fileNo = fileno(fp);
-						dup2(fileNo, STDERR_FILENO);
-						fclose(fp);
+					else {
+						fprintf(stderr, "%s %s\n", "Invalid file:", p);
 						addargs1 = 0;
+						run1 = 0;
 					}
+				}
+				else if(*p == '2' && *(++p) == '>') {
+					p = strtok(NULL, " ");
+					fp1 = fopen(p, "w");
+					errorRedirect1 = fileno(fp1);
+					addargs1 = 0;
+				}
 
-					else if(*p == '|') {
+				else if(*p == '|') {
+					pipeFlag = 1;
+					addargs1 = 0;
+					addargs2 = 1;
+					// set global substring with all text on right side of |
+					char* temp = strstr(cmd, "|");
+					pipecmd = strtok(temp, " ");
+					int j = 0;
+					while(pipecmd != NULL) {
+						if(*pipecmd == '>') {
+							pipecmd = strtok(NULL, " ");
+							fp2 = fopen(pipecmd, "w");
+							outputRedirect2 = fileno(fp2);
+							addargs2 = 0;
+						}
+						else if(*pipecmd == '<') {
+							pipecmd = strtok(NULL, " ");
+							FILE* fp = fopen(p, "r");
+							if(fp) {
+								char* arg;
+								int a = fscanf(fp, "%s", arg);
+								otherargs[j++] = arg;
+								fclose(fp);
+								addargs2 = 0;
+							}
+							else {
+								fprintf(stderr, "%s %s\n", "Invalid file:", p);
+								addargs2 = 0;
+								run2 = 0;
+							}
+						}
+						else if(*p == '2' && *(++p) == '>') {
+							p = strtok(NULL, " ");
+							fp2 = fopen(p, "w");
+							errorRedirect2 = fileno(fp2);
+							addargs2 = 0;
+						}
+
+						if(addargs2) {
+							otherargs[j++] = pipecmd;
+						}
+						pipecmd = strtok(NULL, " ");
+					}
+					otherargs[j] = NULL;
+				}
+
+				if(addargs1) {
+					myargs[i++] = p;
+				}
+				p = strtok(NULL, " ");
+			}	
+
+			myargs[i] = NULL;
+			if(run1) {
+				cpid = fork();
+				if(cpid == 0) { // child 1
+					if(pipeFlag) {
 						setsid();
 						close(pipefd[0]);
 						dup2(pipefd[1], STDOUT_FILENO);
-						pipeFlag = 1;
-						addargs1 = 0;
-						// set global substring with all text on right side of |
-						char* temp = strstr(cmd, "|");
-						pipecmd = strtok(temp, " ");
-
 					}
-
-					if(addargs1) {
-						myargs[i++] = p;
+					if(outputRedirect1 > 0) {
+						dup2(outputRedirect1, STDOUT_FILENO);
+						fclose(fp1);
+					}		
+					if(errorRedirect1 > 0) {
+						dup2(errorRedirect1, STDERR_FILENO);
+						fclose(fp1);
 					}
-					p = strtok(NULL, " ");
-				}	
-				myargs[i] = NULL;
-				if(run1) {
 					if(execvp(myargs[0], myargs) < 0) {
-						printf("%s: %s: %s\n", "yash", myargs[0], "command not found");
+						fprintf(stderr, "%s: %s: %s\n", "yash", myargs[0], "command not found");
 					}
 				}
-			}
-			else {	// Parent
-				if(pipeFlag) { 
-					cpid2 = fork();
-					if(cpid2 == 0) { // child 2
-						// use global substring and loop through and put in args array
-						close(pipefd[1]);
-						dup2(pipefd[0], STDIN_FILENO);
-						run2 = 1;
-						addargs2 = 1;
-						char **myargs = (char**) malloc(sizeof(char*) * 10);
-						int i = 0;
-						char *p = strtok(cmd, " ");
-				
-						while(p != NULL) {
-							if(*p == '>') {
-								p = strtok(NULL, " ");
-								FILE* fp = fopen(p, "w");
-								fileNo = fileno(fp);
-								dup2(fileNo, STDOUT_FILENO);
-								fclose(fp);
-								addargs2 = 0;
-							}
-							else if(*p == '<') {
-								p = strtok(NULL, " ");
-								FILE* fp = fopen(p, "r");
-								if(fp) {
-									//fileNo = fileno(fp);
-									//dup2(fileNo, STDIN_FILENO);
-									char* arg;
-									int a = fscanf(fp, "%s", arg);
-									myargs[i++] = arg;
-									fclose(fp);
-									addargs2 = 0;
-								}
-								else {
-									fprintf(stderr, "%s %s\n", "Invalid file:", p);
-									addargs2 = 0;
-									run2 = 0;
-								}
-							}
-							else if(*p == '2' && *(++p) == '>') {
-								p = strtok(NULL, " ");
-								FILE* fp = fopen(p, "w");
-								fileNo = fileno(fp);
-								dup2(fileNo, STDERR_FILENO);
-								fclose(fp);
-								addargs2 = 0;
-							}
-
-							if(addargs2) {
-								myargs[i++] = p;
-							}
-							p = strtok(NULL, " ");
-						}	
-						myargs[i] = NULL;
+				else {
+					if(pipeFlag) {
 						if(run2) {
-							if(execvp(myargs[0], myargs) < 0) {
-								printf("%s: %s: %s\n", "yash", myargs[0], "command not found");
+							cpid2 = fork();
+							if(cpid2 == 0) { // child 2
+								setpgid(0, cpid);
+								dup2(pipefd[0], STDIN_FILENO);
+								close(pipefd[1]);
+								if(outputRedirect2 > 0) {
+									dup2(outputRedirect2, STDOUT_FILENO);
+									fclose(fp2);
+								}
+								if(errorRedirect2 > 0) {
+									dup2(errorRedirect2, STDERR_FILENO);
+									fclose(fp2);
+								}
+								if(execvp(otherargs[0], otherargs) < 0) {
+									fprintf(stderr, "%s: %s: %s\n", "yash", otherargs[0], "command not found");
+								}
+							}
+							else {
+								wait(NULL);
 							}
 						}
 					}
-					else {
-						pipeFlag = 0;
-						wait(NULL);
-					}
-				}
-				if(run1) {
 					wait(NULL);
 				}
-				
 			}
-				
 		}
 	}
 }
